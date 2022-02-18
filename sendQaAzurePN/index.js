2b179605f57ecd2c2fbc86efca8a314681ad63e9
Irfan Krijestorac <irfan.krijestorac@buffl.be>
2022-02-18 18:35:51 +0100

    Another dummy change

--- sendQaAzurePN/index.js
+++ sendQaAzurePN/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Goedemiddag, " + name + ". This HTTP triggered function executed successfully."
+        ? "Goedemiddag, " + name + ". Irfan triggered HTTP function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
