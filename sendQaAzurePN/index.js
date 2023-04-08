ef8095c1fb36dc6b2dc6c853abddbccbfa92e265
Irfan Krijestorac <irfan.krijestorac@buffl.be>
2023-04-08 15:25:12 +0200

    Database URL moved to configuration file

--- sendQaAzurePN/index.js
+++ sendQaAzurePN/index.js
@@ -1,10 +1,34 @@
+const { MongoClient } = require('mongodb');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
+    let responseMessage = name
         ? "Goedemiddag, " + name + ". Irfan triggered HTTP function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    // Beginning of MongoDB read
+    const directConnectionClient = await MongoClient.connect(
+        process.env.DATABASE_URL,
+      { useNewUrlParser: true }
+    ).catch(err => {
+      context.log(err);
+    });
+    const directDBConnection = directConnectionClient.db();
+
+    try {
+    	const usersCount = await directDBConnection
+            .collection('users')
+            .countDocuments();
+        context.log(usersCount);
+
+        responseMessage = `${responseMessage}\nNumber of users is: ${usersCount}`
+
+	} finally {
+		directConnectionClient.close();
+    }
+    // End of MongoDB read
 
     context.res = {
         // status: 200, /* Defaults to 200 */
